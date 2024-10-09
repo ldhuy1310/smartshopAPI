42a8eb1828799b716f0c0f3d02dce74c685ac099
ldhuy96 <ldhuy.1310@gmail.com>
2024-10-09 22:42:55 +0700

    Search for products on Google Shopping page

--- app.py
+++ app.py
@@ -59,52 +59,57 @@ async def count(req):
 #         return response.json({"Error": str(ex)})
 
 
-def get_url(product_name):
-    product_name = product_name.replace(' ', '%20').replace("#", "%23")
-    template = f"https://www.google.com/search?q={product_name}"
-    url = template.format(product_name)
+def get_url(keyword):
+    keyword = keyword.replace(' ', '%20').replace("#", "%23")
+    template = f"https://www.google.com/search?q={keyword}&tbm=shop&tbs=p_ord:r"
+    url = template.format(keyword)
     return url
 
 
 @app.route('/search')
 async def search(req):
     try:
-        key_search = "shopee " + "#" + req.args.get("q", "").replace(" ", "")
-        url_search = "https://www.google.com/search?q=" + parse.quote(key_search)
+        key_search = "shopee " + req.args.get("q", "fptplay box")
+        url_search = "https://www.google.com/search?q={}&tbm=shop&tbs=p_ord:r".format(parse.quote(key_search))
         driver = app.ctx.chrome_driver
         driver.get(url_search)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        results = soup.find_all('div', class_='MjjYud')
+        results = soup.find_all('div', class_='sh-dgr__content')
         lst_out = []
         for index, result in enumerate(results):
-            title_tag = result.find(class_="LC20lb")
-            description_tag = result.find(class_="VwiC3b")
-            img_tag = result.find('img', {'class': 'VeBrne'})
-            avg_rating_tag = result.find(class_="yi40Hd")
-            total_rating_tag = result.find(class_="RDApEe")
-            price_tag = result.find(class_="LI0TWe")
+            title_tag = result.find(class_="tAxDx")
+            description_tag = result.find(class_="vEjMR")
+            avg_rating_tag = result.find(class_="Rsc7Yb")
+            e_commerce_platform_tag = result.find(class_="IuHnof")
+            div_tag_pre_rating_tag = result.find('div', class_='uqAnbd')
+            total_rating = div_tag_pre_rating_tag.next_sibling.strip() if \
+                (div_tag_pre_rating_tag and div_tag_pre_rating_tag.next_sibling) else ""
+            price_tag = result.find(class_="OFFNJ")
+            href_tag = result.find('a', class_='xCpuod')
+            product_n_img_tag = result.find('div', class_='ArOc1c')
+            product_n_img = product_n_img_tag.find('img') if product_n_img_tag else None
 
-            img = img_tag.get('src').split(",")[-1] if img_tag else ""
+            product_id = product_n_img['id'] if product_n_img else ""
+            img_src = product_n_img['src'] if product_n_img else ""
             title = title_tag.text if title_tag else ""
             description = description_tag.text if description_tag else ""
             avg_rating = avg_rating_tag.text if avg_rating_tag else ""
-            total_rating = total_rating_tag.text if total_rating_tag else ""
+            e_commerce_platform = e_commerce_platform_tag.text if e_commerce_platform_tag else ""
             price = price_tag.text if price_tag else ""
-            href_value = ""
-            href_tag = result.find('a', attrs={'jsname': 'UWckNb'})
-            if href_tag:
-                href_value = href_tag.get('href')
+            href_value = href_tag.get('href').replace(u"/url?url=", "") if href_tag else ""
 
             if title:
                 lst_out.append({
+                    "id": product_id,
                     "title": title,
                     "description": description,
-                    "img": img,
+                    "img": img_src,
                     "avg_rating": avg_rating,
-                    "total_ratign": total_rating,
+                    "total_rating": total_rating,
                     "price": price,
                     "href_value": href_value,
                     "qrcode": generate_qr_code(href_value) if href_value else "",
+                    "e_commerce_platform": e_commerce_platform,
                 })
 
         return response.json({"data": lst_out})
@@ -116,4 +121,4 @@ async def search(req):
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=app.config.PORT, workers=app.config.WORKER, debug=app.config.DEBUG,
             access_log=app.config.ACCESS_LOG,
-            auto_reload=True)
+            auto_reload=False)
